d892ef9ae0c3de4e048e142cf787ba9e48dd7bcc
Jason Wan <jason.wan@ucdconnect.ie>
2024-12-08 10:40:33 +0000

    Day8

---
+++ Day8/Part2.py
@@ -0,0 +1,77 @@
+antennaMap = []
+antiNodes = set()
+antennas = set()
+
+with open("C:\\Users\\J\\source\\repos\\aoc-2024\\Day8\\dayEightInput.txt") as file:
+    for line in file:
+        antennaMap.append(list(line.strip()))
+        for point in antennaMap[len(antennaMap)-1]:
+            if point not in antennas and point != '.':
+                antennas.add(point)
+
+def getLocations(point: chr):
+    locations = []
+
+    for y, row in enumerate(antennaMap):
+        for x, pt in enumerate(row):
+            if pt == point:
+                locations.append(str(y) + "," + str(x))
+
+    return locations
+
+def getYXDelta(higherPoint, lowerPoint):
+    point1 = stringLocationToIntList(higherPoint)
+    point2 = stringLocationToIntList(lowerPoint)
+
+    return [point1[0] - point2[0], point1[1] - point2[1]]
+    
+def isInBounds(y, x, matrix):
+    return y >= 0 and x >= 0 and y < len(matrix) and x < len(matrix[0])
+
+def stringLocationToIntList(location):
+    return [int(x) for x in location.split(",")]
+
+def intLocationToString(list):
+    return str(list[0]) + "," + str(list[1])
+
+for antenna in antennas:
+    locationsOfAntenna = getLocations(antenna)
+    for i in range(0, len(locationsOfAntenna)):
+        for j in range(i+1, len(locationsOfAntenna)):
+            delta = getYXDelta(locationsOfAntenna[i], locationsOfAntenna[j])
+
+            hp = stringLocationToIntList(locationsOfAntenna[i])
+
+            while isInBounds(hp[0], hp[1], antennaMap):
+                hp[0] = hp[0] + delta[0]
+                hp[1] = hp[1] + delta[1]
+
+                if isInBounds(hp[0], hp[1], antennaMap):
+                    antiNodes.add(intLocationToString(hp))
+
+            hp = stringLocationToIntList(locationsOfAntenna[i])
+
+            while isInBounds(hp[0], hp[1], antennaMap):
+                hp[0] = hp[0] - delta[0]
+                hp[1] = hp[1] - delta[1]
+
+                if isInBounds(hp[0], hp[1], antennaMap):
+                    antiNodes.add(intLocationToString(hp))
+
+            lp = stringLocationToIntList(locationsOfAntenna[j])
+            while isInBounds(lp[0], lp[1], antennaMap):
+                lp[0] = lp[0] - delta[0]
+                lp[1] = lp[1] - delta[1]
+
+                if isInBounds(lp[0], lp[1], antennaMap):
+                    antiNodes.add(intLocationToString(lp))
+
+            lp = stringLocationToIntList(locationsOfAntenna[j])
+            while isInBounds(lp[0], lp[1], antennaMap):
+                lp[0] = lp[0] + delta[0]
+                lp[1] = lp[1] + delta[1]
+
+                if isInBounds(lp[0], lp[1], antennaMap):
+                    antiNodes.add(intLocationToString(lp))
+
+print(len(antiNodes))
