3c931c9092250bf8a615468998b211771b36c101
Jason W <jwan8890@gmail.com>
2024-12-14 00:07:23 +0000

    Day11Part2

--- Day11/Part2.py
+++ Day11/Part2.py
@@ -2,20 +2,20 @@ stones = {3279:1, 998884:1, 1832781:1, 517:1, 8:1, 18864:1, 28:1, 0:1}
 
 blinks = 75
 
-for i in range(0, blinks):
+for blink in range(0, blinks):
     newDictionary = {}
-    print(i)
+    print(blink)
     for stone in stones:
-        for _ in range(0, stones[stone]):
-            if stone == 0:
-                newDictionary[1] = newDictionary.get(1, 0) + 1
-            elif len(str(stone)) % 2 == 0:
-                stoneString = str(stone)
-                firstPart, secondPart = int(stoneString[:len(stoneString)//2]), int(stoneString[len(stoneString)//2:])
-                newDictionary[firstPart] = newDictionary.get(firstPart, 0) + 1
-                newDictionary[secondPart] = newDictionary.get(secondPart, 0) + 1
-            else:
-                newDictionary[stone*2024] = newDictionary.get(stone*2024, 0) + 1
+        stoneCount = stones[stone]
+        if stone == 0:
+            newDictionary[1] = newDictionary.get(1, 0) + stoneCount
+        elif len(str(stone)) % 2 == 0:
+            stoneString = str(stone)
+            firstPart, secondPart = int(stoneString[:len(stoneString)//2]), int(stoneString[len(stoneString)//2:])
+            newDictionary[firstPart] = newDictionary.get(firstPart, 0) + stoneCount
+            newDictionary[secondPart] = newDictionary.get(secondPart, 0) + stoneCount
+        else:
+            newDictionary[stone*2024] = newDictionary.get(stone*2024, 0) + stoneCount
     
     stones = newDictionary
 
